6ce2701f78469983167b5eb373af278751c3acf5
BRENOLIVEIRApng <brenoreisrv@gmail.com>
2026-01-10 05:44:21 -0300

    inicial

---
+++ erp-backend/src/main/java/com/zyloerp/ZyloErpApplication.java
@@ -0,0 +1,13 @@
+package com.zyloerp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZyloErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZyloErpApplication.class, args);
+	}
+
+}
---
+++ erp-backend/src/test/java/com/zyloerp/ZyloErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyloerp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZyloErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
